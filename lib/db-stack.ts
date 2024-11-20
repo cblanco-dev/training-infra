398332ec8cc6cd1d8213a6eef1534534f2dab115
Camilo Blanco <camilowhite7@gmail.com>
2024-11-20 17:17:56 -0500

    cambio para single table en infraestructura

--- lib/db-stack.ts
+++ lib/db-stack.ts
@@ -3,46 +3,33 @@ import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class DynamoDbStack extends cdk.Stack {
-  public readonly petsTable: dynamodb.Table;
-  public readonly foundationsTable: dynamodb.Table;
-
+  public readonly petsFoundationsTable: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    this.foundationsTable = new dynamodb.Table(this, 'FoundationsTable', {
-      partitionKey: { name: 'foundationId', type: dynamodb.AttributeType.STRING },
+    this.petsFoundationsTable = new dynamodb.Table(this, 'petsFoundationsTable', {
+      partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    this.petsTable = new dynamodb.Table(this, 'PetsTable', {
-      partitionKey: { name: 'foundationId', type: dynamodb.AttributeType.STRING },
-      sortKey: { name: 'petId', type: dynamodb.AttributeType.STRING },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
-
-    this.petsTable.addGlobalSecondaryIndex({
+    this.petsFoundationsTable.addGlobalSecondaryIndex({
       indexName: 'AnimalTypeIndex',
       partitionKey: { name: 'animalType', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'breed', type: dynamodb.AttributeType.STRING },
     });
 
-    this.petsTable.addGlobalSecondaryIndex({
+    this.petsFoundationsTable.addGlobalSecondaryIndex({
       indexName: 'PetNameIndex',
-      partitionKey: { name: 'foundationId', type: dynamodb.AttributeType.STRING },
+      partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'name', type: dynamodb.AttributeType.STRING },
     });
 
-    new cdk.CfnOutput(this, 'PetsTableName', {
-      value: this.petsTable.tableName,
-      exportName: 'PetsTableName',
-    });
-
-    new cdk.CfnOutput(this, 'FoundationsTableName', {
-      value: this.foundationsTable.tableName,
-      exportName: 'FoundationsTableName',
+    new cdk.CfnOutput(this, 'petsFoundationsTableName', {
+      value: this.petsFoundationsTable.tableName,
+      exportName: 'petsFoundationsTableName',
     });
   }
 }
